15b20ba9b3bdfa618094ece87f4419ade02a59ad
roy-ol <rendra.hmd@gmail.com>
2024-01-07 10:35:37 +0700

    refresh1

--- app/api_node/apiDripInject.php
+++ app/api_node/apiDripInject.php
@@ -50,6 +50,11 @@ if($arrData){
 }
 
 
+//====================================================
+//===========2 - Tanggal aktual(NR_date)==============
+//====================================================
+
+
 //===========Jadwal mingguan==============
 //===========Jadwal mingguan==============
 $arrData = false;
@@ -68,43 +73,47 @@ WHERE nr.id=nrw.id_role and id_node = :id_node AND
 LIMIT 1;   "; 
 $arrData = $cNode->ambil1Row($sSQL,$param);
 // == pause == ementara baris bawah pemeriksaan ada nya order atau tidak
-if(!$arrData){
-  // $cNode->dieJsonNone(); 
-  $cNode->dieJsonNoneCheckUpdate(); 
-  // $cNode->dieJsonNoneTime(); 
+if($arrData){ 
+  //id,id_perusahaan,pola,flag,exeval,satuan,reff_node,ref_n1,ref_n2,ref_n4,
+  // ref_n3,ref_n5,relay,repeater,limval0,limval1,keterangan,updater 
+
+  $id  = $arrData['id']; 
+  $id_nrw  = $arrData['id_nrw']; 
+  $flag  = $arrData['flag'];
+  $pola  = $arrData['pola'];  
+  $exeval  = $arrData['exeval']; 
+  $exe_v1  = $arrData['exe_v1']; 
+  $exe_v2  = $arrData['exe_v2']; 
+  $satuan  = $arrData['satuan']; 
+  $reff_node  = $arrData['reff_node']; 
+  $ref_n1  = $arrData['ref_n1']; 
+  $ref_n2  = $arrData['ref_n2']; 
+  $ref_n3  = $arrData['ref_n3']; 
+  $ref_n4  = $arrData['ref_n4']; 
+  $ref_n5  = $arrData['ref_n5'];  
+  $relay  = $arrData['relay'];
+  $repeater  = $arrData['repeater']; 
+  $limval0  = $arrData['limval0'];
+  $limval1  = $arrData['limval1'];
+  // $keterangan  = $arrData['keterangan']; 
+  // $istoday = $arrData['today']; 
+
+  $idInsert = $cNode->log_eksekutor(null,$id_nrw,$relay,$exeval,$exe_v1,$exe_v2);      
+  $respons['id_log']=intval($idInsert); // id log eksekutor yang baru dibuat di tabel log
+  $respons['f']=20; //flag ada respons untuk aktuator action 
+  // $respons['sleep']=$sleeptime ; //isian bila ada setting sleep berubah
+  $respons['exeval']=$exeval ; // bisa jadi nilai menit / nantinya ml liter setelah kalibrasi
+  $respons["exe_v1"]=$exe_v1 ; // 23-12-2023 nilai sebagai target EC larutan (ppm = * 500)
+
+  $cNode->dieJsonOkTime($respons); 
+
 }
-//id,id_perusahaan,pola,flag,exeval,satuan,reff_node,ref_n1,ref_n2,ref_n4,
-// ref_n3,ref_n5,relay,repeater,limval0,limval1,keterangan,updater 
 
-$id  = $arrData['id']; 
-$id_nrw  = $arrData['id_nrw']; 
-$flag  = $arrData['flag'];
-$pola  = $arrData['pola'];  
-$exeval  = $arrData['exeval']; 
-$exe_v1  = $arrData['exe_v1']; 
-$exe_v2  = $arrData['exe_v2']; 
-$satuan  = $arrData['satuan']; 
-$reff_node  = $arrData['reff_node']; 
-$ref_n1  = $arrData['ref_n1']; 
-$ref_n2  = $arrData['ref_n2']; 
-$ref_n3  = $arrData['ref_n3']; 
-$ref_n4  = $arrData['ref_n4']; 
-$ref_n5  = $arrData['ref_n5'];  
-$relay  = $arrData['relay'];
-$repeater  = $arrData['repeater']; 
-$limval0  = $arrData['limval0'];
-$limval1  = $arrData['limval1'];
-// $keterangan  = $arrData['keterangan']; 
-// $istoday = $arrData['today']; 
 
-$idInsert = $cNode->log_eksekutor(null,$id_nrw,$relay,$exeval,$exe_v1,$exe_v2);      
-$respons['id_log']=intval($idInsert); // id log eksekutor yang baru dibuat di tabel log
-$respons['f']=20; //flag ada respons untuk aktuator action 
-// $respons['sleep']=$sleeptime ; //isian bila ada setting sleep berubah
-$respons['exeval']=$exeval ; // bisa jadi nilai menit / nantinya ml liter setelah kalibrasi
-$respons["exe_v1"]=$exe_v1 ; // 23-12-2023 nilai sebagai target EC larutan (ppm = * 500)
+//====================================================
+//==== 4 - Tanggal terbaru range aktual(NR_date) =====
+//====================================================
 
-$cNode->dieJsonOkTime($respons); 
-
+$cNode->dieJsonNoneCheckUpdate(); 
 
 ?>
