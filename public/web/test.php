b05f2fc3e481008dccbcc8f28e126038b4fc68a2
roy-ol <rendra.hmd@gmail.com>
2024-04-29 16:03:49 +0700

    tes2904241603

--- public/web/test.php
+++ public/web/test.php
@@ -1,4 +1,12 @@
 <?php 
   require_once __DIR__ . '/fungsi/koneksi_umum.php';  
-  splashBerhasil("on Developing");  
+  
+  // splashBerhasil("on Developing");  
 ?>
+
+<button onclick="tes1()">tes alert</button>
+<script>
+  function tes1(){
+    alert("testing alert js");
+  }
+</script>
