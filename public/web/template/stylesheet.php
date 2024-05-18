d0af83a636ac37580ed7bab07d2f2091ba41e7f0
roy-ol <rendra.hmd@gmail.com>
2024-05-18 13:41:55 +0700

    adminLTEinit

---
+++ public/web/template/stylesheet.php
@@ -0,0 +1,9 @@
+
+    <!-- Bootstrap & AdminLTE CSS -->
+    
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons --> 
+    <link rel="stylesheet" href="../../adminlte/plugins/fontawesome-free/css/all.min.css"> 
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../../adminlte/dist/css/adminlte.min.css"> 
